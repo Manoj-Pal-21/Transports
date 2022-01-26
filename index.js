fb80a5e5f2cd26a0deffee1c0355610f2f7a19cb
Manoj <manojpal211999@gmail.com>
2022-01-26 16:15:30 +0530

    removed backend folder

--- index.js
+++ index.js
@@ -22,39 +22,9 @@ app.use(require("./router/auth"));
 // const DB = process.env.DATABASE;
 
 const PORT = process.env.PORT || 5000;
-
-//Routing
  
-app.get("/Aboutus", (req, res) => {
-    res.send(`Hello world about us from the server`);
-});
-
-app.get("/Ourservices", (req, res) => {
-    res.send(`Hello world our services from the server`);
-});
-
-app.get("/Ourclients", (req, res) => {
-    res.send(`Hello world our clients from the server`);
-});
-
-app.get("/Contactus", (req, res) => {
-    res.send(`Hello world contact us from the server`);
-});
-
-app.get("/Booking", (req, res) => {
-    res.send(`Hello world Booking from the server`);
-});
-
-app.get("/Userbooking", (req, res) => {
-    res.send(`Hello world userBooking from the server`);
-});
-
-app.get("/Signin", (req, res) => {
-    res.send(`Hello world login from the server`);
-});
-
-app.get("/Signup", (req, res) => {
-    res.send(`Hello world signup from the server`);
+app.get("/ping", (req, res) => {
+    res.send(`<h3>Pong</h3>`);
 });
 
 if (process.env.NODE_ENV == "production") {
