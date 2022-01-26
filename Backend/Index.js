7139563af41768f085df648e927c6b328488e66d
Manoj <manojpal211999@gmail.com>
2022-01-26 15:55:08 +0530

    connect frontend to backend

--- Backend/Index.js
+++ Backend/Index.js
@@ -60,7 +60,7 @@ app.get("/Signup", (req, res) => {
 if (process.env.NODE_ENV == "production") {
     app.use(express.static("./client/build"));
     const path = require("path");
-    app.get("*",(req,res) => {
+    app.get("/*",(req,res) => {
         res.sendFile(path.resolve(__dirname,"client","build","index.html"), (err) => {
             if(err) res.status(500).send(err);
         });
