8fa5c2399e69dd135cb55c9112238e3ccc6ac656
Manoj <manojpal211999@gmail.com>
2022-01-27 16:11:43 +0530

    add cost

--- frontend/src/Component/Bookingdetails.js
+++ frontend/src/Component/Bookingdetails.js
@@ -53,58 +53,59 @@ const Bookingdetails = () => {
                                 <th> VEHICLES </th>
                                 <th> FROM </th>
                                 <th> TO </th>
+                                <th> COST </th>
                             </tr>
                         </thead>
 
                         <tbody>
                             {
                                 bookingData.length
-                                    ?
-                                    bookingData.map((data) => {
-                                        return (
-                                            <tr key={uuidv4()}>
-                                                <td data-title="NAME">
-                                                    {data.name ? data.name : "N/A"}
-                                                </td>
-                                                <td data-title="LASTNAME">
-                                                    {data.lastname ? data.lastname : "N/A"}
-                                                </td>
-                                                <td data-title="PHONE">
-                                                    {data.phone ? data.phone : "N/A"}
-                                                </td>
-                                                <td data-title="ADDRESS">
-                                                    {data.address ? data.address : "N/A"}
-                                                </td>
-                                                <td data-title="CITY">
-                                                    {data.city ? data.city : "N/A"}
-                                                </td>
-                                                <td data-title="VEHICLES">
-                                                    {data.vehicles ? data.vehicles : "N/A"}
-                                                </td>
-                                                <td data-title="FROM">
-                                                    {data.from ? data.from : "N/A"}
-                                                </td>
-                                                <td data-title="TO">
-                                                    {data.to ? data.to : "N/A"}
-                                                </td>
-                                                <td data-title="COST">
-                                                    {data.cost ? data.cost : "N/A"}
-                                                </td>
-                                            </tr>
-                                        )
-                                    })
-                                    :
-                                    <tr>
-                                        <td data-title="NAME"> N/A </td>
-                                        <td data-title="LASTNAME"> N/A </td>
-                                        <td data-title="PHONE"> N/A </td>
-                                        <td data-title="ADDRESS"> N/A </td>
-                                        <td data-title="CITY"> N/A </td>
-                                        <td data-title="VEHICLES"> N/A </td>
-                                        <td data-title="FROM"> N/A </td>
-                                        <td data-title="TO"> N/A </td>
-                                        <td data-title="COST"> N/A </td>
-                                    </tr>
+                                ?
+                                bookingData.map((data) => {
+                                    return (
+                                        <tr key={uuidv4()}>
+                                            <td data-title="NAME">
+                                                {data.name ? data.name : "N/A"}
+                                            </td>
+                                            <td data-title="LASTNAME">
+                                                {data.lastname ? data.lastname : "N/A"}
+                                            </td>
+                                            <td data-title="PHONE">
+                                                {data.phone ? data.phone : "N/A"}
+                                            </td>
+                                            <td data-title="ADDRESS">
+                                                {data.address ? data.address : "N/A"}
+                                            </td>
+                                            <td data-title="CITY">
+                                                {data.city ? data.city : "N/A"}
+                                            </td>
+                                            <td data-title="VEHICLES">
+                                                {data.vehicles ? data.vehicles : "N/A"}
+                                            </td>
+                                            <td data-title="FROM">
+                                                {data.from ? data.from : "N/A"}
+                                            </td>
+                                            <td data-title="TO">
+                                                {data.to ? data.to : "N/A"}
+                                            </td>
+                                            <td data-title="COST">
+                                                {data.cost ? data.cost : "N/A"}
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                                :
+                                <tr>
+                                    <td data-title="NAME"> N/A </td>
+                                    <td data-title="LASTNAME"> N/A </td>
+                                    <td data-title="PHONE"> N/A </td>
+                                    <td data-title="ADDRESS"> N/A </td>
+                                    <td data-title="CITY"> N/A </td>
+                                    <td data-title="VEHICLES"> N/A </td>
+                                    <td data-title="FROM"> N/A </td>
+                                    <td data-title="TO"> N/A </td>
+                                    <td data-title="COST"> N/A </td>
+                                </tr>
                             }
 
                         </tbody>
